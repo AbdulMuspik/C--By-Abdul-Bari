e1301862672722c1d78721b90217d20a97a048a2
AbdulMuspik <abdulmuspik20@gmail.com>
2024-05-26 23:23:06 +0530

    Initial Commit

---
+++ Myfirst/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Myfirst
+//
+//  Created by MUSFIK on 21/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Musfik!\n";
+    return 0;
+}
