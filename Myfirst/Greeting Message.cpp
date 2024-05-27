70a47d866c1cd539c2605db3d95b20d76a541fc8
AbdulMuspik <abdulmuspik20@gmail.com>
2024-05-27 16:25:57 +0530

    Greeting Message

---
+++ Myfirst/Greeting Message.cpp
@@ -0,0 +1,17 @@
+//
+//  Greeting Message.cpp
+//  Myfirst
+//
+//  Created by MUSFIK on 27/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "May I Know Your Name ? \n";
+    getline(cin,name);
+    cout<<"Welcome "<<name;
+    cout<<endl;
+}
