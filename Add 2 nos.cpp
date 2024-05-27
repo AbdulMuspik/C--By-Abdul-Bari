3f27f23c9a6b1d0256731afc74f30490810aca33
AbdulMuspik <abdulmuspik20@gmail.com>
2024-05-27 16:05:09 +0530

    Add 2 Nos

---
+++ Add 2 nos.cpp
@@ -0,0 +1,21 @@
+//
+//  Add 2 nos.cpp
+//  Myfirst
+//
+//  Created by MUSFIK on 27/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout << "Enter the first number : ";
+    cin >>a;
+    cout << "Enter the second number : ";
+    cin >>b;
+    c=a+b;
+    cout << "The the addition of the two numbers is "<< c;
+    cout <<endl;
+    return 0;
+}
