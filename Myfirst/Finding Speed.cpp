c9fe9577de0f89b13e665bdb748365b801490c60
AbdulMuspik <abdulmuspik20@gmail.com>
2024-05-29 05:00:53 +0530

    Course updated

---
+++ Myfirst/Finding Speed.cpp
@@ -0,0 +1,23 @@
+//
+//  Finding Speed.cpp
+//  Myfirst
+//
+//  Created by MUSFIK on 29/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float s,d,t;
+    
+    cout <<"Enter the value of distance : ";
+    cin >>d;
+    
+    cout <<"Enter the value of time : ";
+    cin >>t;
+    
+    s = d/t;
+    
+    cout <<"The required value of speed is : "<<s<<" m/s."<<endl;
+}
