ecdbbbef63c03f07ee8333672afa076e6eac0c80
AbdulMuspik <abdulmuspik20@gmail.com>
2024-05-28 06:30:22 +0530

    Code Updated

---
+++ Myfirst/Area of a triangle.cpp
@@ -0,0 +1,21 @@
+//
+//  Area of a triangle.cpp
+//  Myfirst
+//
+//  Created by MUSFIK on 28/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float a,l,b;
+    cout <<"Enter the lenght of the triangle : ";
+    cin >>l;
+    cout <<"Enter the breadth of the triangle : ";
+    cin >>b;
+    a=l*b/2;
+    cout <<"The area of the triangle is : "<<a<<endl;
+    return 0;
+}
